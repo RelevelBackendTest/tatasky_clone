c48541587ce9a1848d62ce4bfc11d6d54c15bcbc
RelevelBackendTest <87223414+RelevelBackendTest@users.noreply.github.com>
2022-02-13 00:01:24 +0530

    Add files via upload

---
+++ src/test/java/com/relevel/backend/tatasky/TataskyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relevel.backend.tatasky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TataskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
